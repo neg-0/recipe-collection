5d10882bcc04b6d406a5fbfb74ea5fefd7e3d2a6
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-20 15:15:10 -0400

    cleaned up assertions

--- cypress/integration/home/home.spec.js
+++ cypress/integration/home/home.spec.js
@@ -29,10 +29,9 @@ describe("Home page", () => {
         cy.findByRole('textbox', { name: /Recipe name/i }).type(recipeName)
         cy.findByRole('textbox', { name: /instructions/i }).type("1. heat a skillet on medium with a dollop of coconut oil {enter} 2. warm flour tortillas")
 
-        return cy.findByRole('button').click()
-            .then(() => {
-                expect(cy.findByRole('listitem', /tofu scramble tacos/i)).toExist();
-            })
+        cy.findByRole('button').click()
+
+        cy.findByRole('listitem', /tofu scramble tacos/i).should('exist')
     })
 
     it("displays a recipe names for all added recipes", () => {
@@ -50,8 +49,12 @@ describe("Home page", () => {
             cy.findByRole('textbox', { name: /instructions/i }).type(recipe.recipeInstructions)
 
             cy.findByRole('button').click()
-                .then(() => {
-                    expect(cy.findByText(recipe.recipeName)).toExist();
+
+            cy.findByText(recipe.recipeName).should('exist')
+
+        }
+
+        cy.findAllByRole('listitem').should('have.length', recipes.length)
                 })
         }
 
